2ba7d4051607d4d25648bd88084759641029bfe5
Rachid-Abdou <70857456+Rachid-Abdou@users.noreply.github.com>
2022-01-27 14:46:19 +0100

    Update examen.c

--- examen.c
+++ examen.c
@@ -11,15 +11,9 @@ struct person
     char lname[20];
 };
 
-typedef struct date{
-    int jour;
-    int month;
-    int annee;
-} date;
-
 typedef struct ouvrage{
     int cote__;
-    char titre;  
+    char titre[30];
     int nombre_exemplaire_dans_la_bib;
     int nombre_exemplaires_empruntés;
 }ouvrage;
@@ -27,44 +21,134 @@ typedef struct ouvrage{
 typedef struct etudient{
     int matricule;
     char abonnement; // P = Premium   or   C = classique
-    int pénalité;
+    int penalite;
 }etudient;
 
+typedef struct date{
+    int jour;
+    int month;
+    int annee;
+} date;
+
 typedef struct emprunts_des_ouvrages {
-    int matricule_de_étudiant;
+    int matricule_de_etudiant;
     int cote_de_ouvrage_emprunté;
     date date_emprunt;
     date date_retour;
 }emprunts_des_ouvrages;
 
 
-void Ajouter_un_étudiants(etudient list[]){
-    
+void Ajouter_un_etudiant(etudient list_of_etudient[] , int size) {
+    char f,m;
+    int i;
+
+    do{
+        int flag=0;
+        printf("\n\t\t\t\t Ajout d'un etudiant\n\t\t\t\t~~~~~~~~~~~~~~~~~~~~~");
+        printf("\n\tLe matricule d'etudiant  ");
+        scanf("%d", &list_of_etudient[size].matricule);
+        printf("\t~~~~~~~~~~~~~~~~");
+        for(i=0; i>=size-1; i++){
+            if(list_of_etudient[size].matricule==list_of_etudient[i].matricule){
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==0){
+            printf("\n\t preciser le type d'abonnement d'etudiant (p:pour le type premium || c:pour le type classic) ");
+            scanf("%s", &list_of_etudient[size].abonnement);
+            printf("\n\t entrer le nombre des penalites");
+            scanf("%d", &list_of_etudient[size].penalite);
+            size++;
+
+            do{
+                printf("\n voulez vous l'enregistrer : (y/n) ");
+                scanf("%s", &m);
+                if(m=='y'||m=='Y'){
+                        printf("voulez vous un autre etudiant \n");
+                        scanf("%s", &f);
+                }
+                else{
+                    printf("\n s'il vous plait entre y ou Y pour enregistrer l'information");
+                }
+            }while(m=='n'||m=='N');
+            
+            if(flag!=0){
+                printf("\n l'etudiant existe deja \n"); 
+            }
+        }
+    }while(f=='y'||m=='Y');
 }
 
+
+void Ajouter_un_ouvrage(ouvrage list[1000], int size){
+    char f,m;
+    int  i;
+    
+    do{    
+        int flag=0;
+        printf("\n\n Ajout d'un ouvrage \n\n");
+        printf("titre: ");
+        scanf("%s", list[size].titre);
+
+        for( i=0; i>=size-1; i++){
+            if(list[size].titre==list[i].titre){
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==0){
+            printf("\n la cote ");
+            scanf("%d", &list[size].cote__);
+            printf("\n le nombre que voulez vous l'ajouter ");
+            scanf("%d", &list[size].nombre_exemplaire_dans_la_bib);
+            size++;
+            do{
+                printf("\n\n voulez vous l'enregistrer : (y/n) ");
+                scanf("%s", &m);
+                if(m=='y'||m=='Y'){
+                        printf("voulez vous un autre ouvrage");
+                        scanf("%s", &f);
+                }
+                else{
+                    printf("\n s'il vous plait entre y ou Y pour enregistrer l'information");
+                }
+            }while(m=='n'||m=='N');
+
+            if(flag!=0){
+                printf("\n l'ouvrage existe deja ");
+            }
+        }    
+    }while(f=='y'||m=='Y');
+}
 
 
 int main(){
 
+    ouvrage list_of_ouvrage[10000];
     etudient list_of_etudient[10000];
-
-    list_of_etudient[0].matricule = 1234;
-    list_of_etudient[0].abonnement = 'e';
-    list_of_etudient[0].pénalité = 2;
-
-    FILE *ouvrage_file;
-    FILE *etudient_file;
-    FILE *emprunts_file;
-
-    ouvrage_file = fopen("ouvrage_file.dat", "w");
-    etudient_file = fopen("etudient_file.dat", "w");
-    emprunts_file = fopen("emprunts_file.dat", "w");
+    emprunts_des_ouvrages list_of_emprunts[1000];
 
 
+// test function 1:
+    Ajouter_un_ouvrage(list_of_etudient , 2);
+    printf("matricule: %d\n",list_of_etudient[2].matricule);
+    printf("abonnement: %c\n",list_of_etudient[2].abonnement);
+    printf("penality: %d\n",list_of_etudient[2].penalite);
+
+// test function 2:
+    Ajouter_un_ouvrage(list_of_ouvrage , 2);    
+    printf("cote: %d\n",list_of_ouvrage[2].cote__);
+    printf("titre: %s\n",list_of_ouvrage[2].titre);
+    printf("lib: %d\n",list_of_ouvrage[2].nombre_exemplaire_dans_la_bib);
+    printf("no lib: %d\n",list_of_ouvrage[2].nombre_exemplaires_empruntés);
 
 
+/*
     printf("\n\n                                              bienvenue dans votre programme \n");
-    printf("=============================================================================================================================\n\n");
+    printf("================================================================================================================\n\n");
     printf("                                             !!comment nous pouvons t'aider!!\n\n");
     printf("1==>Ajouter des ouvrages \n");
     printf("2==>Consulter la liste de tous les ouvrages \n");
@@ -86,6 +170,12 @@ int main(){
     printf("18==>Définir le maximum d’ouvrage à emprunter \n");
     printf("19====Pour quitter le programme\n\n");
 
+
+
+
+*/
+
+// example for write struct in file:
 
     FILE *outfile;
      
